e53f275168209eea5844c45c3db1ddb2621ddc2e
Esteban Parra <estebanpr014@gmail.com>
2019-11-18 16:30:33 -0500

    Unit testing examples

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -10,23 +10,19 @@ public class Math {
     public double multiply(double a, double b) {
 
         double result = 0.0;
-        if (b > 1.0) {
-            result = a * b;
-        } else {
-            result = a;
-        }
-
+        result = a * b;
         return result;
     }
 
     public double add(double a, double b) {
-
         return a + b;
     }
 
-    public double divide(double a, double b) {
-
-        return a / b;
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0.0){
+            throw new ArithmeticException();
+        }
+        return a/b;
     }
 
 
--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -10,13 +10,13 @@ public class MathTest {
 //    Therefore, something like test1 , testmethod would not be recognized
 
     @Test
-    public void addTest() throws Exception {
+    public void addtest() throws Exception {
         Math tester = new Math(); // MyClass is tested
 
         // assert statements
 
         assertEquals("10 + 0 must be 10", 10.0, tester.add(10.0, 0.0), 0.0);
-        assertEquals("10 + 0 must be 10",0.0, tester.add(10.0, -10.0), 0.0);
+        assertEquals("10 + -10 must be 0.0",0.0, tester.add(10.0, -10.0), 0.0);
 
     }
 
@@ -24,28 +24,26 @@ public class MathTest {
     public void multiplyTest() throws Exception {
         Math tester = new Math();
 
-        assertEquals(" 10.0 * 2.0 must be 20.0 ", 20.0, tester.multiply(10.0, 2.0));
-        assertEquals(" 10.0 * 0.5 must be 5.0 ",5.0, tester.multiply(10.0, 0.5));
+        assertEquals(" 10.0 * 2.0 must be 20.0 ", 20.0, tester.multiply(10.0, 2.0), 0.0);
+        assertEquals(" 10.0 * 0.5 must be 5.0 ",5.0, tester.multiply(10.0, 0.5), 0.0);
 
     }
 
-//    @Test
-//    public void divisionTest() throws Exception {
-//        Math tester = new Math();
-//
-//        //assertEquals(" 10.0 / 0 must return exception ", , tester.divide(10.0, 0), 0.1);
-//        assertEquals(5.0, tester.divide(10.0, 0.5), 0.1, " 10.0 / 0.5 must be 200 ");
-//        assertEquals(5.0, tester.divide(10.0, 3), 0.1, " 10.0 / 3 must be 200 ");
-//
-//    }
-//
-//    @Test
-//    public void divideByZeroTest() throws Exception{
-//        Math tester = new Math();
-//        Exception exception = assertThrows(ArithmeticException.class, () ->
-//                tester.divide(1, 0));
-//        assertEquals("/ by zero", exception.getMessage());
-//    }
+    @Test
+    public void divisionTest() throws Exception {
+        Math tester = new Math();
+
+        //assertEquals(" 10.0 / 0 must return exception ", , tester.divide(10.0, 0), 0.1);
+        assertEquals(" 10.0 / 0.5 must be 20", 20, tester.divide(10.0, 0.5), 0.1);
+        assertEquals(3.3, tester.divide(10.0, 3), 0.1);
+
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divideByZeroTest() throws Exception{
+        Math tester = new Math();
+        tester.divide(1, 0);
+    }
 
 
 }
